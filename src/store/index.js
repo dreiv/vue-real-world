cf19c405d0bb5a1bf5336feaae7f552cda966ec7
Andrei Voicu <dreivoicu@gmail.com>
2020-08-17 14:22:19 +0300

    access state and derived state

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,31 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue'
+import Vuex from 'vuex'
 
-Vue.use(Vuex);
+Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    user: { id: 'abc123', name: 'Andrei Voicu' },
+    categories: [
+      'sustainability',
+      'nature',
+      'animal welfare',
+      'housing',
+      'education',
+      'food',
+      'community'
+    ],
+    events: [
+      { id: 1, title: '...', organizer: '...' },
+      { id: 2, title: '...', organizer: '...' },
+      { id: 3, title: '...', organizer: '...' },
+      { id: 4, title: '...', organizer: '...' }
+    ]
+  },
   mutations: {},
   actions: {},
-  modules: {}
-});
+  modules: {},
+  getters: {
+    getEventById: state => id => state.events.find(event => event.id === id)
+  }
+})
--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,9 +1,22 @@
 <template>
-  <h1>Create Event</h1>
+  <div>
+    <h1>Create Event, {{ user.name }}</h1>
+    <p>This event was created by {{ user.id }}</p>
+    <ul>
+      <li v-for="category in categories" :key="category">{{ category }}</li>
+    </ul>
+  </div>
 </template>
 
 <script>
-export default {}
+import { mapState, mapGetters } from 'vuex'
+
+export default {
+  computed: {
+    ...mapGetters(['getEventById']),
+    ...mapState(['user', 'categories'])
+  }
+}
 </script>
 
 <style scoped></style>
